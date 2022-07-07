aa2ca9f51f5b982f559de2726b99603b01c8338f
Vitalii Denysyuk <denvit1@gmail.com>
2022-07-08 01:56:45 +0300

    start

---
+++ src/components/MainInput.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+const MainInput = () => {
+    return (
+        <div className="MainInputBlock">
+            <input className="MainInput" type="text"></input>
+            <button className="ButtonAdd">+</button>
+        </div>
+    )
+}
+
+export default MainInput;
